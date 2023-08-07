5d4725deec4463153e88b0dbb4a65b5dd0bd491f
Daniel Rey <=>
2023-08-07 18:27:28 +0800

    Pag di nag error himala

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
 	plugins: [react()],
-	optimizeDeps: {
-		// include: ['linked-dep'],
-	},
-	build: {
-		commonjsOptions: {
-			transformMixedEsModules: true,
-			// include: [/linked-dep/, /node_modules/],
-		},
-	},
 });
