c9c67b7de39d9c83c8a0aba78a3e1b78c2d259a3
Daniel Rey <reyvillanuevadaniel@gmail.com>
2023-08-23 16:41:47 +0800

    add something new

--- src/components/dashboard/Header.tsx
+++ src/components/dashboard/Header.tsx
@@ -5,37 +5,37 @@ import SideNavigation from "../reuseable/SideNavigation";
 import { useState } from "react";
 
 const Header = () => {
-	const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
-	return (
-		<main className="flex relative flex-col">
-			<section className="flex w-full flex-col">
-				<nav className="flex justify-center items-center gap-y-2 md:hidden py-4 flex-col fixed left-0 right-0 top-0 bg-primary w-full">
-					<div className="flex justify-evenly items-center px-5 lg:p-3 bg-primary text-white container gap-x-5">
-						<div className="flex items-center justify-center flex-1">
-							<a href="/" className="font-bold">
-								NACT
-							</a>
-						</div>
+  const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
+  return (
+    <main className="flex relative flex-col">
+      <section className="flex w-full flex-col">
+        <nav className="flex justify-center items-center gap-y-2 md:hidden py-4 flex-col fixed left-0 right-0 top-0 bg-primary w-full">
+          <div className="flex justify-evenly items-center px-5 lg:p-3 bg-primary text-white container gap-x-5">
+            <div className="flex items-center justify-center flex-1">
+              <a href="/" className="font-bold">
+                NACT
+              </a>
+            </div>
 
-						<div className="md:hidden">
-							<DrawerRight
-								isDrawerOpen={isDrawerOpen}
-								setIsDrawerOpen={setIsDrawerOpen}
-							/>
-						</div>
-					</div>
-				</nav>
+            <div className="md:hidden">
+              <DrawerRight
+                isDrawerOpen={isDrawerOpen}
+                setIsDrawerOpen={setIsDrawerOpen}
+              />
+            </div>
+          </div>
+        </nav>
 
-				<div className="w-full md:w-[65%] lg:w-[62%] xl:w-[73%] flex flex-col mt-16 md:mt-5 md:ml-0">
-					<div className="w-full flex flex-col items-center justify-center">
-						<Search isDrawerOpen={isDrawerOpen} />
-						<SideNavigation />
-						<Outlet />
-					</div>
-				</div>
-			</section>
-		</main>
-	);
+        <div className="w-full flex items-center flex-col justify-center relative">
+          <SideNavigation />
+          <div className="flex flex-col items-center justify-center absolute right-0 top-2 md:w-2/3 lg:w-[80%]">
+            <Search isDrawerOpen={isDrawerOpen} />
+            <Outlet />
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Header;
--- src/components/reuseable/SideNavigation.tsx
+++ src/components/reuseable/SideNavigation.tsx
@@ -4,106 +4,113 @@ import { useSelectedStore } from "@/utils/HomePageState";
 import logo from "@/assets/logo.svg";
 
 const SideNavigation = () => {
-	const [selected] = useSelectedStore((state) => [state.selected]);
+  const [selected] = useSelectedStore((state) => [state.selected]);
 
-	const buttonClass =
-		"flex gap-x-3 py-2 mb-2 mx-3 px-2 text-sm rounded-md items-center transition-opacity duration-300";
-	const hoverButtonClass =
-		"hover:text-blue-500 hover:font-semibold hover:bg-white";
+  const buttonClass =
+    "flex gap-x-3 py-2 mb-2 mx-3 px-2 text-sm rounded-md items-center transition-opacity duration-300";
+  const hoverButtonClass =
+    "hover:text-blue-500 hover:font-semibold hover:bg-white";
 
-	return (
-		<aside className="max-w-[370px] w-full fixed top-0 md:w-[30%] lg:w-full right-0 bottom-0 drop-shadow-md hidden md:flex bg-primary flex-col pt-5 pb-2 z-[100] md:items-center">
-			<Link to="/" className="mx-5">
-				<img
-					className="mask mask-hexagon bg-white w-14 p-3"
-					src={logo}
-					id="home-id"
-					alt="NACT logo"
-				/>
-			</Link>
-			<ul className="mt-10 flex flex-col justify-between h-full w-full">
-				<div>
-					<li>
-						<Link
-							to="/"
-							className={`${buttonClass} ${
-								selected === ButtonList.Home
-									? "text-primary font-semibold bg-slate-50"
-									: "text-white font-thin"
-							} ${hoverButtonClass}`}>
-							Home
-						</Link>
-					</li>
-					<li>
-						<Link
-							to="/module"
-							className={`${buttonClass} ${
-								selected === ButtonList.Module
-									? "text-primary font-semibold bg-slate-50"
-									: "text-white font-thin"
-							} ${hoverButtonClass}`}>
-							Module
-						</Link>
-					</li>
-					<li>
-						<Link
-							to="/costumer"
-							className={`${buttonClass} ${
-								selected === ButtonList.Costumer
-									? "text-primary font-semibold bg-slate-50"
-									: "text-white font-thin"
-							} ${hoverButtonClass}`}>
-							Customer
-						</Link>
-					</li>
-					<li>
-						<Link
-							to="/supplier"
-							className={`${buttonClass} ${
-								selected === ButtonList.Supplier
-									? "text-primary font-semibold bg-slate-50"
-									: "text-white font-thin"
-							} ${hoverButtonClass}`}>
-							Supplier Management
-						</Link>
-					</li>
-					<li>
-						<Link
-							to="/sales-agent"
-							className={`${buttonClass} ${
-								selected === ButtonList.SalesAgent
-									? "text-primary font-semibold bg-slate-50"
-									: "text-white font-thin"
-							} ${hoverButtonClass}`}>
-							Sales Agent
-						</Link>
-					</li>
-					<li>
-						<Link
-							to="/inventory-officer"
-							className={`${buttonClass} ${
-								selected === ButtonList.InventoryOfficer
-									? "text-primary font-semibold bg-slate-50"
-									: "text-white font-thin"
-							} ${hoverButtonClass}`}>
-							Inventory Officer
-						</Link>
-					</li>
-				</div>
-				<li>
-					<Link
-						to="/account"
-						className={`${buttonClass} ${
-							selected === ButtonList.Account
-								? "text-primary font-semibold bg-slate-50"
-								: "text-white font-thin"
-						} ${hoverButtonClass}`}>
-						Account
-					</Link>
-				</li>
-			</ul>
-		</aside>
-	);
+  return (
+    <aside className="max-w-[370px] w-full fixed top-0 md:w-[30%] lg:w-full left-0 bottom-0 drop-shadow-md hidden md:flex bg-primary flex-col pt-5 pb-2 z-[100] md:items-center">
+      <Link to="/" className="mx-5">
+        <img
+          className="mask mask-hexagon bg-white w-14 p-3"
+          src={logo}
+          id="home-id"
+          alt="NACT logo"
+        />
+      </Link>
+      <ul className="mt-10 flex flex-col justify-between h-full w-full">
+        <div>
+          <li>
+            <Link
+              to="/"
+              className={`${buttonClass} ${
+                selected === ButtonList.Home
+                  ? "text-primary font-semibold bg-slate-50"
+                  : "text-white font-thin"
+              } ${hoverButtonClass}`}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/module"
+              className={`${buttonClass} ${
+                selected === ButtonList.Module
+                  ? "text-primary font-semibold bg-slate-50"
+                  : "text-white font-thin"
+              } ${hoverButtonClass}`}
+            >
+              Module
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/costumer"
+              className={`${buttonClass} ${
+                selected === ButtonList.Costumer
+                  ? "text-primary font-semibold bg-slate-50"
+                  : "text-white font-thin"
+              } ${hoverButtonClass}`}
+            >
+              Customer
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/supplier"
+              className={`${buttonClass} ${
+                selected === ButtonList.Supplier
+                  ? "text-primary font-semibold bg-slate-50"
+                  : "text-white font-thin"
+              } ${hoverButtonClass}`}
+            >
+              Supplier Management
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/sales-agent"
+              className={`${buttonClass} ${
+                selected === ButtonList.SalesAgent
+                  ? "text-primary font-semibold bg-slate-50"
+                  : "text-white font-thin"
+              } ${hoverButtonClass}`}
+            >
+              Sales Agent
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/inventory-officer"
+              className={`${buttonClass} ${
+                selected === ButtonList.InventoryOfficer
+                  ? "text-primary font-semibold bg-slate-50"
+                  : "text-white font-thin"
+              } ${hoverButtonClass}`}
+            >
+              Inventory Officer
+            </Link>
+          </li>
+        </div>
+        <li>
+          <Link
+            to="/account"
+            className={`${buttonClass} ${
+              selected === ButtonList.Account
+                ? "text-primary font-semibold bg-slate-50"
+                : "text-white font-thin"
+            } ${hoverButtonClass}`}
+          >
+            Account
+          </Link>
+        </li>
+      </ul>
+    </aside>
+  );
 };
 
 export default SideNavigation;
