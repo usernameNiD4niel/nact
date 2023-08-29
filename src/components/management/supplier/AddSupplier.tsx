3b23af0e13dd1d5ebbf90aca3479116397b337a4
Daniel Rey <=>
2023-08-29 20:52:09 +0800

    fix issue: add supplier content center

--- src/components/management/supplier/AddSupplier.tsx
+++ src/components/management/supplier/AddSupplier.tsx
@@ -5,17 +5,17 @@ import { Link, Outlet } from "react-router-dom";
 
 const AddSupplier = () => {
 	return (
-		<div className={`${headerBackClass} lg:flex flex-col items-center`}>
+		<div className={`${headerBackClass}`}>
 			<HeaderWithBack text="Add Supplier" route="/supplier" />
-			<div className="py-2 mt-12 px-4 lg:w-[60%]">
+			<div className="py-2 mt-12 px-4 lg:w-full">
 				<p className="text-sm py-2">Select customer type</p>
-				<hr className="mb-5" />
-				<ul className="space-y-2 lg:flex lg:space-y-0 lg:gap-x-4 flex-col">
+				<hr className="" />
+				<ul className="space-y-0 lg:flex lg:space-y-0 lg:gap-x-4 flex-col">
 					{CustomerType.map((value, index) => (
 						<li key={index}>
 							<Link
 								to={`/supplier/add/${value.route}`}
-								className="flex flex-col md:p-3 p-2 pb-0 border-b-black border-opacity-20 border-b-[1px] w-full">
+								className="flex flex-col min-h-16 justify-center pb-0 border-b-black border-opacity-20 border-b-[1px] w-full">
 								<h3 className="font-bold text-primary">{value.title}</h3>
 								<p className="font-thin text-xs">{value.description}</p>
 							</Link>
