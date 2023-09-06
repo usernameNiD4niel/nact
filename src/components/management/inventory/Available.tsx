4cd14cc41aa2ad1fd59b1f9986d62a01b5cce5b8
Daniel Rey <reyvillanuevadaniel@gmail.com>
2023-09-06 17:55:42 +0800

    hirap sir

--- src/components/management/inventory/Available.tsx
+++ src/components/management/inventory/Available.tsx
@@ -8,82 +8,84 @@ import { HiOutlinePlus } from "react-icons/hi2";
 import { Link } from "react-router-dom";
 
 const Available = (): JSX.Element => {
-	const [setActiveTab] = useInventoryState((state) => [state.setActiveTab]);
+  const [setActiveTab] = useInventoryState((state) => [state.setActiveTab]);
 
-	const [isShowingFilter, setIsShowingFilter] = useState<boolean>(false);
+  const [isShowingFilter, setIsShowingFilter] = useState<boolean>(false);
 
-	useEffect(() => setActiveTab(0), []);
-	return (
-		<>
-			<div className="md:px-10 w-full space-y-5 px-5 py-6">
-				<SearchWithFilter placeHolder="Search Inventory" />
-				<AddButton />
-				<TableMutator setIsShowingFilter={setIsShowingFilter} />
-				<DisplayInventoryData />
-			</div>
-			{isShowingFilter && <Filter setIsShowingFilter={setIsShowingFilter} />}
-		</>
-	);
+  useEffect(() => setActiveTab(0), []);
+  return (
+    <>
+      <div className="md:px-10 w-full space-y-5 px-5 py-6">
+        <SearchWithFilter placeHolder="Search Inventory" />
+        <AddButton />
+        <TableMutator setIsShowingFilter={setIsShowingFilter} />
+        <DisplayInventoryData />
+      </div>
+      {isShowingFilter && <Filter setIsShowingFilter={setIsShowingFilter} />}
+    </>
+  );
 };
 
 type TableMutatorProps = {
-	setIsShowingFilter: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsShowingFilter: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 const TableMutator: FC<TableMutatorProps> = ({ setIsShowingFilter }) => {
-	return (
-		<div className="w-full flex items-center justify-between flex-wrap">
-			<div>
-				<DynamicDropdown
-					dropdownText="Sort"
-					dropDownItems={[
-						"Product Name",
-						"City",
-						"State",
-						"Quantity",
-						"Depot",
-						"Price",
-					]}
-				/>
-			</div>
-			<div className="items-center hidden lg:flex">
-				<DynamicDropdown
-					dropdownText="Product Name"
-					dropDownItems={["20 STD - CW", "40 HC - CW", "20 STD - OT"]}
-				/>
-				<DynamicDropdown dropdownText="City" dropDownItems={["Chicago"]} />
-				<DynamicDropdown dropdownText="State" dropDownItems={["USA"]} />
-				<DynamicDropdown dropdownText="Quantity" dropDownItems={["12 PCS"]} />
-				<DynamicDropdown dropdownText="Depot" dropDownItems={["Depot"]} />
-				<DynamicDropdown dropdownText="Price" dropDownItems={["$1,250"]} />
-			</div>
-			<button
-				className="text-gray-700 lg:hidden"
-				type="button"
-				onClick={() => setIsShowingFilter((prev) => !prev)}>
-				Filter
-			</button>
-		</div>
-	);
+  return (
+    <div className="w-full flex items-center justify-between flex-wrap">
+      <div>
+        <DynamicDropdown
+          dropdownText="Sort"
+          dropDownItems={[
+            "Product Name",
+            "City",
+            "State",
+            "Quantity",
+            "Depot",
+            "Price",
+          ]}
+        />
+      </div>
+      <div className="items-center hidden lg:flex">
+        <DynamicDropdown
+          dropdownText="Product Name"
+          dropDownItems={["20 STD - CW", "40 HC - CW", "20 STD - OT"]}
+        />
+        <DynamicDropdown dropdownText="City" dropDownItems={["Chicago"]} />
+        <DynamicDropdown dropdownText="State" dropDownItems={["USA"]} />
+        <DynamicDropdown dropdownText="Quantity" dropDownItems={["12 PCS"]} />
+        <DynamicDropdown dropdownText="Depot" dropDownItems={["Depot"]} />
+        <DynamicDropdown dropdownText="Price" dropDownItems={["$1,250"]} />
+      </div>
+      <button
+        className="text-gray-700 lg:hidden"
+        type="button"
+        onClick={() => setIsShowingFilter((prev) => !prev)}
+      >
+        Filter
+      </button>
+    </div>
+  );
 };
 
 const DisplayInventoryData = (): JSX.Element => {
-	return (
-		<section className="w-full flex items-center justify-center flex-col gap-y-2">
-			<TableSixCol />
-		</section>
-	);
+  return (
+    <section className="w-full flex items-center justify-center flex-col gap-y-2">
+      <TableSixCol />
+    </section>
+  );
 };
 
 const AddButton = () => {
-	return (
-		<Link
-			to="/inventory/add"
-			className="absolute right-2 bottom-4 md:right-10 hover:opacity-90 flex rounded-full items-center justify-center gap-x-2 w-14 h-14 bg-primary md:w-32 text-white text-2xl">
-			<HiOutlinePlus />
-			<span className="hidden md:block text-sm">INVENTORY</span>
-		</Link>
-	);
+  return (
+    <Link
+      to="/inventory/add"
+      className="absolute right-2 bottom-4 md:right-10 hover:opacity-90 flex rounded-full items-center justify-center gap-x-2 w-14 h-14 bg-primary md:w-32 text-white text-2xl"
+    >
+      <HiOutlinePlus />
+      <span className="hidden md:block text-sm">INVENTORY</span>
+    </Link>
+  );
 };
 
 export default Available;
