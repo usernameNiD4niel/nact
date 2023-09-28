1ccbb966c5b50640a3a5f3280e743bd966a30414
Daniel Rey <reyvillanuevadaniel@gmail.com>
2023-09-28 14:40:27 +0800

    hello sir

--- src/api/supplier.ts
+++ src/api/supplier.ts
@@ -3,55 +3,66 @@ import Cookies from "js-cookie";
 
 const token = Cookies.get("token");
 const getSupplierTableData = async () => {
-	console.log("token, ", token);
+  console.log("token, ", token);
 
-	const response = await fetch(
-		`https://flask-service.gi2fod26lfct0.ap-southeast-1.cs.amazonlightsail.com/supplier`,
-		{
-			headers: {
-				"Content-Type": "application/json",
-				Authorization: "Bearer " + token,
-			},
-		},
-	);
+  const response = await fetch(
+    `https://flask-service.gi2fod26lfct0.ap-southeast-1.cs.amazonlightsail.com/supplier`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+    }
+  );
 
-	if (response.ok) {
-		const data: Promise<SupplierDataProps> = response.json();
-		return (await data).suppliers;
-	} else {
-		throw new Error(response.arrayBuffer.toString());
-	}
+  if (response.ok) {
+    const data: Promise<SupplierDataProps> = response.json();
+    return (await data).suppliers;
+  } else {
+    throw new Error(response.arrayBuffer.toString());
+  }
 };
 
 type ResponseAddShipping = {
-	message: string;
+  message: string;
 };
 
-const addShippingSupplier = async (shipping: ShippingFormProps) => {
-	console.log("Shipping ", shipping);
-	console.log("token supp: ", token);
+const addShippingSupplier = async (
+  shipping: ShippingFormProps,
+  setValidation: React.Dispatch<React.SetStateAction<string>>,
+  setMessage: React.Dispatch<React.SetStateAction<string>>
+) => {
+  console.log("Shipping ", JSON.stringify(shipping));
+  console.log("token supp: ", token);
 
-	const response = await fetch(
-		`https://flask-service.gi2fod26lfct0.ap-southeast-1.cs.amazonlightsail.com/api/supplier/add/shipping`,
-		{
-			method: "post",
-			headers: {
-				"Content-Type": "application/json",
-				Authorization: "Bearer " + token,
-			},
-			body: JSON.stringify(shipping),
-		},
-	);
+  const response = await fetch(
+    `https://flask-service.gi2fod26lfct0.ap-southeast-1.cs.amazonlightsail.com/api/supplier/add/shipping`,
+    {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+      body: JSON.stringify(shipping),
+    }
+  );
 
-	if (response.ok) {
-		const data: Promise<ResponseAddShipping> = await response.json();
-		console.log("data hehhe", data);
-		return (await data).message;
-	} else {
-		const data: Promise<ResponseAddShipping> = await response.json();
-		console.log("error hehhe", data);
-		return JSON.stringify(data, null, 2);
-	}
+  if (response.ok) {
+    const data: Promise<ResponseAddShipping> = await response.json();
+    console.log("may supplier ba", data);
+
+    const { message } = await data;
+    setMessage(message);
+    setValidation("success");
+    return true;
+  } else {
+    const data: Promise<ResponseAddShipping> = await response.json();
+    setValidation("error");
+    console.log("error hehhe", data);
+    const { message } = await data;
+    setMessage(message);
+    return false;
+  }
 };
 
 export { getSupplierTableData, addShippingSupplier };
