f3c333050dfd831a48e55ddaa5412c907dd9e666
Daniel Rey <=>
2023-10-11 21:42:00 +0800

    added padding to supplier table

--- src/components/management/supplier/List.tsx
+++ src/components/management/supplier/List.tsx
@@ -5,73 +5,69 @@ import { DataTable } from "./helper/data-table";
 import { columns } from "./helper/columns";
 import { getSupplierTableData } from "@/api/supplier";
 import { Link } from "react-router-dom";
-import { mobileColumn } from "./helper/mobile-column";
+// import { mobileColumn } from "./helper/mobile-column";
 
 const List = () => {
-  const [setTab] = useInventoryState((state) => [state.setActiveTab]);
+	const [setTab] = useInventoryState((state) => [state.setActiveTab]);
 
-  const [supplier, setSupplier] = useState<Payment[]>([]);
-  const [isFetching, setIsFetching] = useState(true);
+	const [supplier, setSupplier] = useState<Payment[]>([]);
+	const [isFetching, setIsFetching] = useState(true);
 
-  const datas = async () => {
-    const d = await getSupplierTableData(setIsFetching);
-    setSupplier(d);
-  };
+	const datas = async () => {
+		const d = await getSupplierTableData(setIsFetching);
+		setSupplier(d);
+	};
 
-  useEffect(() => {
-    datas();
-    setTab(0);
-  }, []);
+	useEffect(() => {
+		datas();
+		setTab(0);
+	}, []);
 
-  const ContentTable = () => {
-    if (isFetching) {
-      return <div>Fetching data please wait...</div>;
-    }
+	const ContentTable = () => {
+		if (isFetching) {
+			return <div>Fetching data please wait...</div>;
+		}
 
-    if (supplier && supplier.length > 0) {
-      return (
-        <React.Fragment>
-          <div className="hidden md:flex">
-            <DataTable
-              columns={columns}
-              data={supplier}
-              setData={setSupplier}
-            />
-          </div>
-          <div className="md:hidden">
-            <DataTable
-              columns={mobileColumn}
-              data={supplier}
-              setData={setSupplier}
-            />
-          </div>
-        </React.Fragment>
-      );
-    }
+		if (supplier && supplier.length > 0) {
+			return (
+				<React.Fragment>
+					{/* <div className="hidden md:flex w-full"> */}
+					<DataTable columns={columns} data={supplier} setData={setSupplier} />
+					{/* </div> */}
+					{/* <div className="md:hidden w-full">
+						<DataTable
+							columns={mobileColumn}
+							data={supplier}
+							setData={setSupplier}
+						/>
+					</div> */}
+				</React.Fragment>
+			);
+		}
 
-    return (
-      <div>
-        No supplier data yet, <Link to="/supplier/add">create new entry</Link>
-      </div>
-    );
-  };
+		return (
+			<div>
+				No supplier data yet, <Link to="/supplier/add">create new entry</Link>
+			</div>
+		);
+	};
 
-  return (
-    <div className="w-full">
-      <div className="md:px-10 px-2">
-        <div className="mt-36 md:mt-24">
-          <ContentTable />
-        </div>
-      </div>
-      {/* <AddButton /> */}
-      {/* {isShowingFilter && (
+	return (
+		<div className="w-full">
+			<div className="md:px-10 px-5">
+				<div className="mt-36 md:mt-24">
+					<ContentTable />
+				</div>
+			</div>
+			{/* <AddButton /> */}
+			{/* {isShowingFilter && (
         <Filter
           data={SupplierTableData}
           setIsShowingFilter={setIsShowingFilter}
         />
       )} */}
-    </div>
-  );
+		</div>
+	);
 };
 
 export default List;
