acea4d76755509bfb98183a5de3411250d250eb3
Daniel Rey <=>
2023-10-11 21:57:05 +0800

    added a thing

--- src/components/management/supplier/helper/SearchWithFilter.tsx
+++ src/components/management/supplier/helper/SearchWithFilter.tsx
@@ -4,41 +4,40 @@ import { Input } from "@/components/ui/input";
 import FilteringDialog from "./FilteringDialog";
 
 type SearchWithFilterProps = {
-  placeHolder: string;
-  isList: boolean;
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  value: string;
-  setData: React.Dispatch<React.SetStateAction<Payment[]>>;
+	placeHolder: string;
+	isList: boolean;
+	onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+	value: string;
+	setData: React.Dispatch<React.SetStateAction<Payment[]>>;
 };
 
 const SearchWithFilter: FC<SearchWithFilterProps> = ({
-  placeHolder,
-  onChange,
-  value,
-  setData,
+	placeHolder,
+	onChange,
+	value,
+	setData,
 }) => {
-  const handleOnSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-  };
+	const handleOnSubmit = (event: React.FormEvent) => {
+		event.preventDefault();
+	};
 
-  return (
-    <>
-      <form
-        className="w-full flex items-center gap-x-1 justify-center relative"
-        onSubmit={handleOnSubmit}
-      >
-        <Input
-          type="text"
-          placeholder={placeHolder}
-          className="py-6"
-          value={value}
-          onChange={onChange}
-          // className="border-[1px] rounded-lg border-black border-opacity-10 w-full p-3 focus:outline-primary focus:outline-1"
-        />
-        {/* <FilterUI handleFilter={handleFilter} /> */}
-        <FilteringDialog setData={setData} />
-        {/* <FilteringModal /> */}
-        {/* <FilterModal
+	return (
+		<>
+			<form
+				className="w-full flex items-center justify-center relative"
+				onSubmit={handleOnSubmit}>
+				<Input
+					type="text"
+					placeholder={placeHolder}
+					className="py-6"
+					value={value}
+					onChange={onChange}
+					// className="border-[1px] rounded-lg border-black border-opacity-10 w-full p-3 focus:outline-primary focus:outline-1"
+				/>
+				{/* <FilterUI handleFilter={handleFilter} /> */}
+				<FilteringDialog setData={setData} />
+				{/* <FilteringModal /> */}
+				{/* <FilterModal
           contact={contact}
           location={location}
           setContact={setContact}
@@ -47,9 +46,9 @@ const SearchWithFilter: FC<SearchWithFilterProps> = ({
           setSupplier={setSupplier}
           supplier={supplier}
         /> */}
-      </form>
-    </>
-  );
+			</form>
+		</>
+	);
 };
 
 export default SearchWithFilter;
