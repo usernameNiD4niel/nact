58034ffbcc03b33bc50e9f94c380752db13b4915
Daniel Rey <=>
2023-10-17 21:33:55 +0800

    added credentials

--- src/api/login.ts
+++ src/api/login.ts
@@ -29,7 +29,7 @@ export const POST = async ({
 					"Content-Type": "application/json",
 				},
 				body: JSON.stringify(data),
-				credentials: "include",
+				// credentials: "include",
 			},
 		);
 
