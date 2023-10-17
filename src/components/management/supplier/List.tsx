e44cbc90d5cadb72803257f141abeb2e41d22cae
Daniel Rey <=>
2023-10-17 21:27:02 +0800

    ssdsd

--- src/components/management/supplier/List.tsx
+++ src/components/management/supplier/List.tsx
@@ -3,86 +3,81 @@ import React, { useEffect, useState } from "react";
 import { SupplierTableProps } from "@/constants/props";
 import { DataTable } from "./helper/data-table";
 import { columns } from "./helper/columns";
-import { getPaginatedSupplier } from "@/api/supplier";
 import { Link } from "react-router-dom";
 import { mobileColumn } from "./helper/mobile-column";
 
 const List = () => {
-  const [setTab] = useInventoryState((state) => [state.setActiveTab]);
+	const [setTab] = useInventoryState((state) => [state.setActiveTab]);
 
-  const [supplier, setSupplier] = useState<SupplierTableProps[]>([]);
-  const [isFetching, setIsFetching] = useState(true);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [nextPage, setNextPage] = useState<number | null>(2);
-  const [previousPage, setPreviousPage] = useState<number | null>(null);
+	const [supplier, setSupplier] = useState<SupplierTableProps[]>([]);
+	// const [isFetching, setIsFetching] = useState(true);
+	// const [currentPage, setCurrentPage] = useState(1);
+	// const [nextPage, setNextPage] = useState<number | null>(2);
+	// const [previousPage, setPreviousPage] = useState<number | null>(null);
 
-  const datas = async () => {
-    const d = await getPaginatedSupplier(currentPage, setIsFetching);
+	// const datas = async () => {
+	// 	const d = await getPaginatedSupplier(currentPage, setIsFetching);
 
-    setNextPage(d.next_page);
-    setPreviousPage(d.previous_page);
+	// 	setNextPage(d.next_page);
+	// 	setPreviousPage(d.previous_page);
 
-    setSupplier(d.suppliers);
-  };
+	// 	setSupplier(d.suppliers);
+	// };
 
-  useEffect(() => {
-    datas();
-    setTab(0);
-  }, [currentPage]);
+	useEffect(() => {
+		// datas();
+		setTab(0);
+	}, []);
 
-  const ContentTable = () => {
-    if (isFetching) {
-      return <div>Fetching data please wait...</div>;
-    }
+	const ContentTable = () => {
+		// if (isFetching) {
+		// 	return <div>Fetching data please wait...</div>;
+		// }
 
-    if (supplier && supplier.length > 0) {
-      return (
-        <React.Fragment>
-          <div className="hidden md:flex w-full">
-            <DataTable
-              columns={columns}
-              data={supplier}
-              setCurrentPage={setCurrentPage}
-              nextPage={nextPage}
-              previousPage={previousPage}
-            />
-          </div>
-          <div className="md:hidden w-full">
-            <DataTable
-              columns={mobileColumn}
-              data={supplier}
-              setCurrentPage={setCurrentPage}
-              nextPage={nextPage}
-              previousPage={previousPage}
-            />
-          </div>
-        </React.Fragment>
-      );
-    }
+		if (supplier && supplier.length > 0) {
+			return (
+				<React.Fragment>
+					<div className="hidden md:flex w-full">
+						<DataTable
+							columns={columns}
+							data={supplier}
+							setData={setSupplier}
+						/>
+					</div>
+					<div className="md:hidden w-full">
+						<DataTable
+							columns={mobileColumn}
+							data={supplier}
+							setData={setSupplier}
+						/>
+					</div>
+				</React.Fragment>
+			);
+		}
 
-    return (
-      <div>
-        No supplier data yet, <Link to="/supplier/add">create new entry</Link>
-      </div>
-    );
-  };
+		return (
+			<div>
+				No supplier data yet, <Link to="/supplier/add">create new entry</Link>
+			</div>
+		);
+	};
 
-  return (
-    <div className="w-full flex items-center justify-center">
-      <div className="md:px-10 px-5 w-full">
-        <div className="mt-36 md:mt-24 w-full">
-          <ContentTable />
-        </div>
-      </div>
-      {/* <AddButton /> */}
-      {/* {isShowingFilter && (
+	return (
+		<div className="w-full flex items-center justify-center">
+			<div className="md:px-10 px-5 w-full">
+				<div className="mt-36 md:mt-24 w-full">
+					<ContentTable />
+				</div>
+			</div>
+			{/* <AddButton /> */}
+			{/* {isShowingFilter && (
         <Filter
           data={SupplierTableData}
           setIsShowingFilter={setIsShowingFilter}
         />
       )} */}
-    </div>
-  );
+		</div>
+	);
 };
 
 export default List;
--- src/components/management/supplier/helper/data-table.tsx
+++ src/components/management/supplier/helper/data-table.tsx
@@ -1,230 +1,229 @@
 import {
-  ColumnDef,
-  ColumnFiltersState,
-  SortingState,
-  flexRender,
-  getCoreRowModel,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  getSortedRowModel,
-  useReactTable,
+	ColumnDef,
+	ColumnFiltersState,
+	SortingState,
+	flexRender,
+	getCoreRowModel,
+	getFilteredRowModel,
+	getPaginationRowModel,
+	getSortedRowModel,
+	useReactTable,
 } from "@tanstack/react-table";
 
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
 } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { Link, useNavigate } from "react-router-dom";
 import { HiOutlinePlus } from "react-icons/hi2";
 import { SupplierTableProps } from "@/constants/props";
 import SearchWithFilter from "./SearchWithFilter";
+import { getPaginatedSupplier } from "@/api/supplier";
 
 interface DataTableProps<TValue> {
-  columns: ColumnDef<SupplierTableProps, TValue>[];
-  data: SupplierTableProps[];
-  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
-  previousPage: number | null;
-  nextPage: number | null;
+	columns: ColumnDef<SupplierTableProps, TValue>[];
+	data: SupplierTableProps[];
+	setData: React.Dispatch<React.SetStateAction<SupplierTableProps[]>>;
 }
 
 export function DataTable<TValue>({
-  columns,
-  data,
-  setCurrentPage,
-  nextPage,
-  previousPage,
+	columns,
+	data,
+	setData,
 }: DataTableProps<TValue>) {
-  const [sorting, setSorting] = React.useState<SortingState>([]);
-  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+	const [sorting, setSorting] = React.useState<SortingState>([]);
+	const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
 
-  const navigate = useNavigate();
+	const navigate = useNavigate();
+	const [counter, setCounter] = useState(10);
+	const [isFetching, setIsFetching] = useState(true);
 
-  const table = useReactTable({
-    data,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    onSortingChange: setSorting,
-    getSortedRowModel: getSortedRowModel(),
-    onColumnFiltersChange: setColumnFilters,
-    getFilteredRowModel: getFilteredRowModel(),
-    state: {
-      sorting,
-      columnFilters,
-    },
-  });
+	const table = useReactTable({
+		data,
+		columns,
+		getCoreRowModel: getCoreRowModel(),
+		getPaginationRowModel: getPaginationRowModel(),
+		onSortingChange: setSorting,
+		getSortedRowModel: getSortedRowModel(),
+		onColumnFiltersChange: setColumnFilters,
+		getFilteredRowModel: getFilteredRowModel(),
+		state: {
+			sorting,
+			columnFilters,
+		},
+	});
 
-  const getValue = () =>
-    (table.getColumn("businessName")?.getFilterValue() as string) ?? "";
+	const getData = async () => {
+		const d = await getPaginatedSupplier(
+			Math.floor(counter / 10),
+			setIsFetching,
+		);
+		setData((prev) => [...prev, ...d.suppliers]);
+		table.setPageSize(counter);
+	};
 
-  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) =>
-    table.getColumn("businessName")?.setFilterValue(event.target.value);
+	useEffect(() => {
+		if (!(data.length <= 10)) {
+			getData();
+		}
+		console.log(isFetching);
+	}, [counter]);
 
-  const handleTableItem = (supplier: string) => {
-    const foundObject = data.find((item) => item.businessName === supplier);
+	const getValue = () =>
+		(table.getColumn("businessName")?.getFilterValue() as string) ?? "";
 
-    navigate(`/supplier/${foundObject?.id}`);
-  };
+	const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+		table.getColumn("businessName")?.setFilterValue(event.target.value);
 
-  // const getState = (supplier: string) => {
-  // 	return data
-  // 		.find((item) => item.businessName === supplier)
-  // 		?.location.split(",")[0]
-  // 		.trim();
-  // };
+	const handleTableItem = (supplier: string) => {
+		const foundObject = data.find((item) => item.businessName === supplier);
 
-  const getLocation = (supplier: string) => {
-    return data.find((item) => item.businessName === supplier)?.location.trim();
-  };
+		navigate(`/supplier/${foundObject?.id}`);
+	};
 
-  return (
-    <div className="w-full">
-      <div className="my-4 w-full space-y-2">
-        <SearchWithFilter
-          placeHolder="Search Supplier"
-          isList={true}
-          value={getValue()}
-          onChange={handleOnChange}
-          data={data}
-        />
-      </div>
-      <div className="rounded-md border">
-        <Table className="w-full">
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                            // eslint-disable-next-line no-mixed-spaces-and-tabs
-                          )}
-                    </TableHead>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                  className="hover:cursor-pointer bg-white hover:bg-zinc-50/10"
-                  onClick={() => handleTableItem(row.getValue("businessName"))}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                      <span className="md:hidden ml-4 flex flex-col">
-                        {cell.id.endsWith("businessName") === true && (
-                          <>{getLocation(row.getValue("businessName"))}</>
-                        )}
-                      </span>
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  No results.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </div>
-      {/* <div className="my-3">
+	// const getState = (supplier: string) => {
+	// 	return data
+	// 		.find((item) => item.businessName === supplier)
+	// 		?.location.split(",")[0]
+	// 		.trim();
+	// };
+
+	const getLocation = (supplier: string) => {
+		return data.find((item) => item.businessName === supplier)?.location.trim();
+	};
+
+	return (
+		<div className="w-full">
+			<div className="my-4 w-full space-y-2">
+				<SearchWithFilter
+					placeHolder="Search Supplier"
+					isList={true}
+					value={getValue()}
+					onChange={handleOnChange}
+					data={data}
+				/>
+			</div>
+			<div className="rounded-md border">
+				<Table className="w-full">
+					<TableHeader>
+						{table.getHeaderGroups().map((headerGroup) => (
+							<TableRow key={headerGroup.id}>
+								{headerGroup.headers.map((header) => {
+									return (
+										<TableHead key={header.id}>
+											{header.isPlaceholder
+												? null
+												: flexRender(
+														header.column.columnDef.header,
+														header.getContext(),
+														// eslint-disable-next-line no-mixed-spaces-and-tabs
+												  )}
+										</TableHead>
+									);
+								})}
+							</TableRow>
+						))}
+					</TableHeader>
+					<TableBody>
+						{table.getRowModel().rows?.length ? (
+							table.getRowModel().rows.map((row) => (
+								<TableRow
+									key={row.id}
+									data-state={row.getIsSelected() && "selected"}
+									className="hover:cursor-pointer bg-white hover:bg-zinc-50/10"
+									onClick={() => handleTableItem(row.getValue("businessName"))}>
+									{row.getVisibleCells().map((cell) => (
+										<TableCell key={cell.id}>
+											{flexRender(
+												cell.column.columnDef.cell,
+												cell.getContext(),
+											)}
+											<span className="md:hidden ml-4 flex flex-col">
+												{cell.id.endsWith("businessName") === true && (
+													<>{getLocation(row.getValue("businessName"))}</>
+												)}
+											</span>
+										</TableCell>
+									))}
+								</TableRow>
+							))
+						) : (
+							<TableRow>
+								<TableCell
+									colSpan={columns.length}
+									className="h-24 text-center">
+									No results.
+								</TableCell>
+							</TableRow>
+						)}
+					</TableBody>
+				</Table>
+			</div>
+			{/* <div className="my-3">
 				<DataTablePagination table={table} />
 			</div> */}
-      {/* <div className="w-full flex items-center justify-center my-4">
-        {Math.floor(table.getFilteredRowModel().flatRows.length / 10) ===
-        Math.floor((counter - 20) / 10) ? (
-          <Button
-            className="bg-transparent text-[#017DC3] px-8 font-bold py-6"
-            disabled
-          >
-            No Item Found
-          </Button>
-        ) : (
-          <Button
-            variant={"noVariant"}
-            className="px-8 font-bold py-6 text-[#017DC3] bg-transparent"
-            onClick={() => {
-              setCurrentPage((prevCount) => prevCount + 1);
-              console.log("dataaa: ", data);
-              // setData([]);
+			<div className="w-full flex items-center justify-center my-4">
+				{Math.floor(table.getFilteredRowModel().flatRows.length / 10) ===
+				Math.floor((counter - 20) / 10) ? (
+					<Button
+						className="bg-transparent text-[#017DC3] px-8 font-bold py-6"
+						disabled>
+						No Item Found
+					</Button>
+				) : (
+					<Button
+						variant={"noVariant"}
+						className="px-8 font-bold py-6 text-[#017DC3] bg-transparent"
+						onClick={() => {
+							// setData([]);
 
-              setCounter((prevCount) => prevCount + 10);
-              console.log("count: ", table.getPageCount());
-              table.setPageSize(counter);
-              // table.setPageIndex(2);
-            }}
-          >
-            Load More
-          </Button>
-        )}
-      </div> */}
-      <div className="flex w-full items-center justify-center space-x-2 py-4">
-        <Button
-          variant="noVariant"
-          size="sm"
-          onClick={() => {
-            if (previousPage) {
-              setCurrentPage(previousPage);
-            }
-            table.previousPage();
-          }}
-          disabled={!previousPage}
-          className="disabled:bg-white bg-[#017DC3] text-white disabled:text-black"
-        >
-          Previous
-        </Button>
-        <Button
-          variant="noVariant"
-          size="sm"
-          onClick={() => {
-            if (nextPage) {
-              setCurrentPage(nextPage);
-            }
-            // setCurrentPage((prevCount) => prevCount + 1);
-            // setCounter((prevCount) => prevCount + 10);
-            table.nextPage();
-          }}
-          disabled={!nextPage}
-          className="disabled:bg-white bg-[#017DC3] text-white disabled:text-black"
-        >
-          Next
-        </Button>
-      </div>
-      <div className="fixed bottom-2 right-4">
-        <Link
-          to="/supplier/add"
-          className="flex gap-1 rounded-full w-14 md:w-auto md:h-auto items-center justify-center text-white p-4 text-2xl h-14 bg-[#017DC3]"
-        >
-          <HiOutlinePlus />
-          <span className="hidden md:block text-sm">SUPPLIER</span>
-        </Link>
-      </div>
-    </div>
-  );
+							setCounter((prevCount) => prevCount + 10);
+							// table.setPageSize(counter);
+							// table.setPageIndex(2);
+						}}>
+						Load More
+					</Button>
+				)}
+			</div>
+			<div className="flex w-full items-center justify-center space-x-2 py-4">
+				<Button
+					variant="noVariant"
+					size="sm"
+					onClick={() => {
+						table.previousPage();
+					}}
+					// disabled={!previousPage}
+					className="disabled:bg-white bg-[#017DC3] text-white disabled:text-black">
+					Previous
+				</Button>
+				<Button
+					variant="noVariant"
+					size="sm"
+					onClick={() => {
+						// setCurrentPage((prevCount) => prevCount + 1);
+						// setCounter((prevCount) => prevCount + 10);
+						table.nextPage();
+					}}
+					// disabled={!nextPage}
+					className="disabled:bg-white bg-[#017DC3] text-white disabled:text-black">
+					Next
+				</Button>
+			</div>
+			<div className="fixed bottom-2 right-4">
+				<Link
+					to="/supplier/add"
+					className="flex gap-1 rounded-full w-14 md:w-auto md:h-auto items-center justify-center text-white p-4 text-2xl h-14 bg-[#017DC3]">
+					<HiOutlinePlus />
+					<span className="hidden md:block text-sm">SUPPLIER</span>
+				</Link>
+			</div>
+		</div>
+	);
 }
