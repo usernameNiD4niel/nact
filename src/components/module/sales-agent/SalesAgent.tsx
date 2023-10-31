5e69d6cfe8aaffa59192f32a9f476d5db1ed5c1f
Daniel Rey <=>
2023-10-31 14:26:20 +0800

    added boilerplate to all of the module atbs

--- src/components/module/sales-agent/SalesAgent.tsx
+++ src/components/module/sales-agent/SalesAgent.tsx
@@ -1,11 +1,78 @@
 import { useInventoryState } from "@/utils/InventoryState";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import AssignRoleCard from "../helper/assign-role-card";
+import DemoteRole from "../helper/demote-role";
 
 const SalesAgent = () => {
 	const [setTab] = useInventoryState((state) => [state.setActiveTab]);
-
+	const [selectedCards, setSelectedCards] = useState<string[]>([]);
 	useEffect(() => setTab(2), []);
-	return <div>SalesAgent</div>;
+
+	return (
+		<div className="flex flex-wrap gap-2 px-5 w-full">
+			<AssignRoleCard
+				birthday="October 25 2000"
+				contact="09876543211"
+				dateCreated="October 24, 2023"
+				fullName="Daniel V. Rey"
+				gender="Male"
+				selectedCards={selectedCards}
+				setSelectedCards={setSelectedCards}
+				key={6}
+			/>
+			<AssignRoleCard
+				birthday="October 25 2000"
+				contact="09876543211"
+				dateCreated="October 24, 2023"
+				fullName="Daniel V. Rey"
+				gender="Male"
+				selectedCards={selectedCards}
+				setSelectedCards={setSelectedCards}
+				key={5}
+			/>
+			<AssignRoleCard
+				birthday="October 25 2000"
+				contact="09876543211"
+				dateCreated="October 24, 2023"
+				fullName="Daniel V. Rey"
+				gender="Male"
+				selectedCards={selectedCards}
+				setSelectedCards={setSelectedCards}
+				key={4}
+			/>
+			<AssignRoleCard
+				birthday="October 25 2000"
+				contact="09876543211"
+				dateCreated="October 24, 2023"
+				fullName="Daniel V. Rey"
+				gender="Male"
+				selectedCards={selectedCards}
+				setSelectedCards={setSelectedCards}
+				key={3}
+			/>
+			<AssignRoleCard
+				birthday="October 25 2000"
+				contact="09876543211"
+				dateCreated="October 24, 2023"
+				fullName="Daniel V. Rey"
+				gender="Male"
+				selectedCards={selectedCards}
+				setSelectedCards={setSelectedCards}
+				key={2}
+			/>
+			<AssignRoleCard
+				birthday="October 25 2000"
+				contact="09876543211"
+				dateCreated="October 24, 2023"
+				fullName="Daniel V. Rey"
+				gender="Male"
+				selectedCards={selectedCards}
+				setSelectedCards={setSelectedCards}
+				key={1}
+			/>
+			{selectedCards.length > 0 && <DemoteRole />}
+		</div>
+	);
 };
 
 export default SalesAgent;
