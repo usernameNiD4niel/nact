d0e7fcdd79a6c84b1eb1ede8ea761597ac8cc6a9
Daniel Rey <reyvillanuevadaniel@gmail.com>
2023-11-29 15:56:22 +0800

    same thing

--- src/components/management/role-management/users/user-management/role-management.tsx
+++ src/components/management/role-management/users/user-management/role-management.tsx
@@ -30,7 +30,7 @@ const RoleManagement: FC<RoleManagementProps> = ({
           disabled={isDisabled}
           checked={status === "active"}
           onCheckedChange={handleOnCheckChange}
-          className="text-green-500"
+          className="bg-green-500 active:border-4 border-x-red-500"
         />
       </div>
       <div className="w-full">
