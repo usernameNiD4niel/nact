0505359f10046e6a8955677c021cbaf837729536
Daniel Rey <=>
2023-11-30 06:11:08 +0800

    oww there's also typo here

--- src/components/management/customer/CustomerManagement.tsx
+++ src/components/management/customer/CustomerManagement.tsx
@@ -7,39 +7,39 @@ import { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 
 const CustumerManagement = () => {
-  const [selected, setSelected] = useSelectedStore((state) => [
-    state.selected,
-    state.setSelected,
-  ]);
+	const [selected, setSelected] = useSelectedStore((state) => [
+		state.selected,
+		state.setSelected,
+	]);
 
-  const [tab] = useInventoryState((state) => [state.tab, state.setActiveTab]);
+	const [tab] = useInventoryState((state) => [state.tab, state.setActiveTab]);
 
-  const role = Cookies.get("role");
+	const role = Cookies.get("role");
 
-  const router = useNavigate();
+	const router = useNavigate();
 
-  useEffect(() => {
-    if (!(role === "customer" || role === "admin")) {
-      router("/");
-    }
-    if (selected !== ButtonList.Custumer) {
-      setSelected(ButtonList.Custumer);
-    }
-  }, []);
+	useEffect(() => {
+		if (!(role === "customer" || role === "admin")) {
+			router("/");
+		}
+		if (selected !== ButtonList.Customer) {
+			setSelected(ButtonList.Customer);
+		}
+	}, []);
 
-  return (
-    <section className="w-full flex flex-col items-center justify-center gap-y-4">
-      <div className="w-full md:w-[70%] lg:w-[80%] z-10 bg-[#1F2123] px-5 mb-0 pt-6 border-t-[#1F2123] border-t-2 fixed top-14 md:top-0">
-        <Tabs
-          activeTabIndex={tab}
-          arrayOfText={["List", "Analytics"]}
-          arrayOfRoutes={["/customer/", "/customer/analytics"]}
-          key={"CustomerTab"}
-        />
-      </div>
-      <Outlet />
-    </section>
-  );
+	return (
+		<section className="w-full flex flex-col items-center justify-center gap-y-4">
+			<div className="w-full md:w-[70%] lg:w-[80%] z-10 bg-[#1F2123] px-5 mb-0 pt-6 border-t-[#1F2123] border-t-2 fixed top-14 md:top-0">
+				<Tabs
+					activeTabIndex={tab}
+					arrayOfText={["List", "Analytics"]}
+					arrayOfRoutes={["/customer/", "/customer/analytics"]}
+					key={"CustomerTab"}
+				/>
+			</div>
+			<Outlet />
+		</section>
+	);
 };
 
 export default CustumerManagement;
