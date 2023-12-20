c8da6b3c63ca9ac86e2cf1e524f924b29a839b2a
Daniel Rey <=>
2023-12-20 23:28:20 +0800

    make the bug free responsive

--- src/components/reuseable/SheetRight.tsx
+++ src/components/reuseable/SheetRight.tsx
@@ -15,12 +15,22 @@ import { ButtonList } from "@/constants/enums";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
 import { Button } from "../ui/button";
 import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
 
 const SheetRight = () => {
 	const [selected] = useSelectedStore((state) => [state.selected]);
 	const publiClassDrawer = "text-white p-2 text-base flex items-center gap-2";
 
-	const role = Cookies.get("role");
+	const access_module = Cookies.get("access_module");
+	const [accessModule, setAccessModule] = useState<string[]>([]);
+
+	useEffect(() => {
+		if (access_module) {
+			const access = JSON.parse(access_module);
+			setAccessModule(access);
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
 	const buttonClass =
 		"flex gap-x-3 py-2 mb-2 font-medium mx-3 px-2 text-md rounded-md items-center transition-opacity duration-300 w-full";
@@ -85,51 +95,51 @@ const SheetRight = () => {
 									buttonList={ButtonList.Home}
 									to="/"
 								/>
-								{role === "admin" && (
-									<SheetCloseComp
-										text="Role Management"
-										to="/role-management"
-										buttonList={ButtonList.RoleManagement}
-									/>
-								)}
-								{role !== "unset" && (
+								{accessModule.map((access) => (
 									<>
-										{(role === "admin" || role === "customer") && (
+										{access === "Role Management" && (
+											<SheetCloseComp
+												text="Role Management"
+												to="/role-management"
+												buttonList={ButtonList.RoleManagement}
+											/>
+										)}
+										{access === "Customer" && (
 											<SheetCloseComp
 												text="Customer"
 												to="/customer"
 												buttonList={ButtonList.Customer}
 											/>
 										)}
-										{(role === "admin" || role === "supplier_chain") && (
+										{access === "Supplier Management" && (
 											<SheetCloseComp
 												text="Supplier Management"
 												to="/supplier"
 												buttonList={ButtonList.Supplier}
 											/>
 										)}
-										{(role === "sales_agent" || role === "admin") && (
+										{access === "Sales Agent" && (
 											<SheetCloseComp
 												text="Sales Agent"
 												to="/sales-agent"
 												buttonList={ButtonList.SalesAgent}
 											/>
 										)}
-										{(role === "admin" || role === "inventory") && (
-											<>
-												<SheetCloseComp
-													text="Inventory Officer"
-													to="/inventory-officer"
-													buttonList={ButtonList.InventoryOfficer}
-												/>
-												<SheetCloseComp
-													text="Inventory"
-													to="/inventory"
-													buttonList={ButtonList.Inventory}
-												/>
-											</>
+										{access === "Inventory Officer" && (
+											<SheetCloseComp
+												text="Inventory Officer"
+												to="/inventory-officer"
+												buttonList={ButtonList.InventoryOfficer}
+											/>
 										)}
-										{(role === "admin" || role === "order_generator") && (
+										{access === "Inventory" && (
+											<SheetCloseComp
+												text="Inventory"
+												to="/inventory"
+												buttonList={ButtonList.Inventory}
+											/>
+										)}
+										{access === "Order Generator" && (
 											<SheetCloseComp
 												text="Order Generator"
 												to="/order-generator"
@@ -137,7 +147,7 @@ const SheetRight = () => {
 											/>
 										)}
 									</>
-								)}
+								))}
 							</p>
 
 							<SheetCloseComp
