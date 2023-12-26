e573a8968f2c7f7219e14c21b699cbed3b10cc7d
Daniel Rey <=>
2023-12-26 23:06:56 +0800

    added the deed

--- src/components/management/role-management/modify-access/update-role.tsx
+++ src/components/management/role-management/modify-access/update-role.tsx
@@ -12,17 +12,42 @@ import {
 	DialogDescription,
 	DialogHeader,
 	DialogTitle,
-	DialogTrigger,
 } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 interface UpdateRoleProps {
 	roles: string[];
+	setUpdateRoleOpen: React.Dispatch<React.SetStateAction<boolean>>;
+	updateRoleOpen: boolean;
 }
 
-export default function UpdateRole({ roles }: UpdateRoleProps) {
+export default function UpdateRole({
+	roles,
+	setUpdateRoleOpen,
+	updateRoleOpen,
+}: UpdateRoleProps) {
+	const [selectedRole, setSelectedRole] = useState("");
+
+	function handleFormSubmit(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+
+		const formData = new FormData(e.currentTarget);
+		console.log(`role : ${formData.get("role")}`);
+		console.log(`new role : ${formData.get("new_role")}`);
+	}
+
+	function handleOnOpeChange(open: boolean) {
+		setUpdateRoleOpen(open);
+	}
+
+	function handleOnValueChange(value: string) {
+		setSelectedRole(value);
+	}
+
 	return (
-		<Dialog>
-			<DialogTrigger>Update</DialogTrigger>
+		<Dialog onOpenChange={handleOnOpeChange} open={updateRoleOpen}>
 			<DialogContent>
 				<DialogHeader>
 					<DialogTitle>Update Role</DialogTitle>
@@ -30,20 +55,32 @@ export default function UpdateRole({ roles }: UpdateRoleProps) {
 						Select a role to update or rename them
 					</DialogDescription>
 				</DialogHeader>
-				<Select>
-					<SelectTrigger /*className="w-[180px]"*/>
-						<SelectValue placeholder="Select a role" />
-					</SelectTrigger>
-					<SelectContent>
-						{roles.map((role) => (
-							<SelectItem value={role} key={role}>
-								{role}
-							</SelectItem>
-						))}
-						{/* <SelectItem value="dark">Dark</SelectItem>
-						<SelectItem value="system">System</SelectItem> */}
-					</SelectContent>
-				</Select>
+				<form onSubmit={handleFormSubmit} className="flex flex-col gap-2">
+					<Select
+						required={true}
+						name="role"
+						onValueChange={handleOnValueChange}>
+						<SelectTrigger /*className="w-[180px]"*/>
+							<SelectValue placeholder="Select a role" />
+						</SelectTrigger>
+						<SelectContent>
+							{roles.map((role) => (
+								<SelectItem value={role} key={role}>
+									{role}
+								</SelectItem>
+							))}
+							{/* <SelectItem value="dark">Dark</SelectItem>
+                            <SelectItem value="system">System</SelectItem> */}
+						</SelectContent>
+					</Select>
+					<Input
+						placeholder="Select role first"
+						name="new_role"
+						required
+						value={selectedRole}
+					/>
+					<Button className="mt-4">Update New Role</Button>
+				</form>
 			</DialogContent>
 		</Dialog>
 	);
--- src/components/management/role-management/modify-access/vertical-option.tsx
+++ src/components/management/role-management/modify-access/vertical-option.tsx
@@ -7,25 +7,39 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { SlOptionsVertical } from "react-icons/sl";
 import UpdateRole from "./update-role";
+import { useState } from "react";
 
 interface VerticalOptionProps {
 	roles: string[];
 }
 
 export default function VerticalOption({ roles }: VerticalOptionProps) {
+	const [updateRoleOpen, setUpdateRoleOpen] = useState(false);
+
+	function handleUpdate() {
+		setUpdateRoleOpen(true);
+	}
+
 	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger asChild>
-				<Button variant={"link"} className="text-xs p-0" type="button">
-					<SlOptionsVertical />
-				</Button>
-			</DropdownMenuTrigger>
-			<DropdownMenuContent>
-				<DropdownMenuItem>
-					<UpdateRole roles={roles} />
-				</DropdownMenuItem>
-				<DropdownMenuItem>Delete</DropdownMenuItem>
-			</DropdownMenuContent>
-		</DropdownMenu>
+		<>
+			<DropdownMenu>
+				<DropdownMenuTrigger asChild>
+					<Button variant={"link"} className="text-xs p-0" type="button">
+						<SlOptionsVertical />
+					</Button>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent>
+					<DropdownMenuItem onClick={handleUpdate}>Update</DropdownMenuItem>
+					<DropdownMenuItem>Delete</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
+			{updateRoleOpen && (
+				<UpdateRole
+					roles={roles}
+					setUpdateRoleOpen={setUpdateRoleOpen}
+					updateRoleOpen={updateRoleOpen}
+				/>
+			)}
+		</>
 	);
 }
