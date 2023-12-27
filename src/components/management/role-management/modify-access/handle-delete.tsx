12256b1da90da9cfc12e0f73bf8f50cff6835413
Daniel Rey <=>
2023-12-27 20:55:31 +0800

    make the UI functional for delete button

---
+++ src/components/management/role-management/modify-access/handle-delete.tsx
@@ -0,0 +1,38 @@
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+
+interface HandleDeleteProps {
+	deleteRoleOpen: boolean;
+	setDeleteRoleOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function HandleDelete({
+	deleteRoleOpen,
+	setDeleteRoleOpen,
+}: HandleDeleteProps) {
+	return (
+		<AlertDialog onOpenChange={setDeleteRoleOpen} open={deleteRoleOpen}>
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+					<AlertDialogDescription>
+						This action cannot be undone. This will permanently delete your
+						account and remove your data from our servers.
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+				<AlertDialogFooter>
+					<AlertDialogCancel>Cancel</AlertDialogCancel>
+					<AlertDialogAction>Continue</AlertDialogAction>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
+	);
+}
--- src/components/management/role-management/modify-access/vertical-option.tsx
+++ src/components/management/role-management/modify-access/vertical-option.tsx
@@ -1,51 +1,63 @@
 import { Button } from "@/components/ui/button";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { SlOptionsVertical } from "react-icons/sl";
 import UpdateRole from "./update-role";
 import { useState } from "react";
+import HandleDelete from "./handle-delete";
 interface VerticalOptionProps {
-  roles: string[];
-  /*refetch: <TPageData>(
+	roles: string[];
+	/*refetch: <TPageData>(
     options?: (RefetchOptions & RefetchQueryFilters<TPageData>) | undefined
   ) => Promise<QueryObserverResult<string[], unknown>>;*/
 }
 
 export default function VerticalOption({
-  roles,
+	roles,
 }: //refetch,
 VerticalOptionProps) {
-  const [updateRoleOpen, setUpdateRoleOpen] = useState(false);
+	const [updateRoleOpen, setUpdateRoleOpen] = useState(false);
+	const [deleteRoleOpen, setDeleteRoleOpen] = useState(false);
 
-  function handleUpdate() {
-    setUpdateRoleOpen(true);
-  }
+	function handleUpdate() {
+		setUpdateRoleOpen(true);
+	}
 
-  return (
-    <>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant={"link"} className="text-xs p-0" type="button">
-            <SlOptionsVertical />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuItem onClick={handleUpdate}>Update</DropdownMenuItem>
-          <DropdownMenuItem>Delete</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-      {updateRoleOpen && (
-        <UpdateRole
-          roles={roles}
-          setUpdateRoleOpen={setUpdateRoleOpen}
-          updateRoleOpen={updateRoleOpen}
-          //refetch={refetch}
-        />
-      )}
-    </>
-  );
+	function handleDelete() {
+		setDeleteRoleOpen(true);
+	}
+
+	return (
+		<>
+			<DropdownMenu>
+				<DropdownMenuTrigger asChild>
+					<Button variant={"link"} className="text-xs p-0" type="button">
+						<SlOptionsVertical />
+					</Button>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent>
+					<DropdownMenuItem onClick={handleUpdate}>Update</DropdownMenuItem>
+					<DropdownMenuItem onClick={handleDelete}>Delete</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
+			{updateRoleOpen && (
+				<UpdateRole
+					roles={roles}
+					setUpdateRoleOpen={setUpdateRoleOpen}
+					updateRoleOpen={updateRoleOpen}
+					//refetch={refetch}
+				/>
+			)}
+			{deleteRoleOpen && (
+				<HandleDelete
+					setDeleteRoleOpen={setDeleteRoleOpen}
+					deleteRoleOpen={deleteRoleOpen}
+				/>
+			)}
+		</>
+	);
 }
