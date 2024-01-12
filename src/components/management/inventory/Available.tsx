250b2691be1ac3f4c7f5a83cc902545375524e80
Daniel Rey <reyvillanuevadaniel@gmail.com>
2024-01-12 22:43:10 +0800

    remove the bug where the expired is declining

--- src/components/management/inventory/Available.tsx
+++ src/components/management/inventory/Available.tsx
@@ -8,59 +8,59 @@ import { getPaginatedData } from "@/api/inventory";
 import { useSearchParams } from "react-router-dom";
 
 const Available = (): JSX.Element => {
-  const [setActiveTab] = useInventoryState((state) => [state.setActiveTab]);
+	const [setActiveTab] = useInventoryState((state) => [state.setActiveTab]);
 
-  const [data, setData] = useState<InventoryData[]>([]);
+	const [data, setData] = useState<InventoryData[]>([]);
 
-  const [clone, setClone] = useState<InventoryData[]>([]);
+	const [clone, setClone] = useState<InventoryData[]>([]);
 
-  const [queryParams] = useSearchParams();
+	const [queryParams] = useSearchParams();
 
-  const shouldRefetch = queryParams.get("shouldRefetch");
+	const shouldRefetch = queryParams.get("shouldRefetch");
 
-  const [nextPageUrl, setNextPageUrl] = useState<number | null>(null);
+	const [nextPageUrl, setNextPageUrl] = useState<number | null>(null);
 
-  const fetchedData = async () => {
-    const data_ = await getPaginatedData();
-    setNextPageUrl(data_.next_page);
-    setData(data_.products);
-    setClone(data_.products);
-  };
+	const fetchedData = async () => {
+		const data_ = await getPaginatedData();
+		setNextPageUrl(data_.next_page);
+		setData(data_.products);
+		setClone(data_.products);
+	};
 
-  useEffect(() => {
-    if (data && data.length > 0) {
-      localStorage.setItem("table_data", JSON.stringify(data));
-    }
-  }, [data]);
-  const tableData = localStorage.getItem("table_data");
+	useEffect(() => {
+		if (data && data.length > 0) {
+			localStorage.setItem("table_data", JSON.stringify(data));
+		}
+	}, [data]);
+	const tableData = localStorage.getItem("table_data");
 
-  useEffect(() => {
-    if (tableData) {
-      if (shouldRefetch && shouldRefetch === "true") {
-        fetchedData();
-      } else {
-        setData(JSON.parse(tableData));
-      }
-    } else {
-      fetchedData();
-      setActiveTab(0);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+	useEffect(() => {
+		if (tableData) {
+			if (shouldRefetch && shouldRefetch === "true") {
+				fetchedData();
+			} else {
+				setData(JSON.parse(tableData));
+			}
+		} else {
+			fetchedData();
+		}
+		setActiveTab(0);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
-  return (
-    <>
-      <div className="w-full flex items-center justify-center">
-        <div className="md:px-10 px-5 w-full">
-          <div className="mt-36 md:mt-24 w-full">
-            <NewDataTable
-              columns={columns}
-              data={data}
-              next_page_url={nextPageUrl}
-              clone={clone}
-              setData={setData}
-            />
-            {/* <div className="w-full md:flex hidden">
+	return (
+		<>
+			<div className="w-full flex items-center justify-center">
+				<div className="md:px-10 px-5 w-full">
+					<div className="mt-36 md:mt-24 w-full">
+						<NewDataTable
+							columns={columns}
+							data={data}
+							next_page_url={nextPageUrl}
+							clone={clone}
+							setData={setData}
+						/>
+						{/* <div className="w-full md:flex hidden">
 							<NewDataTable
 								columns={columns}
 								data={data}
@@ -78,16 +78,16 @@ const Available = (): JSX.Element => {
 								setData={setData}
 							/>
 						</div> */}
-          </div>
-        </div>
-        <AddButton
-          redirectUrl="/inventory/add"
-          textButton="Inventory"
-          key={"InventoryAddTable"}
-        />
-      </div>
-    </>
-  );
+					</div>
+				</div>
+				<AddButton
+					redirectUrl="/inventory/add"
+					textButton="Inventory"
+					key={"InventoryAddTable"}
+				/>
+			</div>
+		</>
+	);
 };
 
 export default Available;
