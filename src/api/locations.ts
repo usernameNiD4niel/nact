681ce8f78e8c1c4f1d0db4455770d4f4c2ede531
Daniel Rey <reyvillanuevadaniel@gmail.com>
2024-01-24 22:33:35 +0800

    created a boilerplate

---
+++ src/api/locations.ts
@@ -0,0 +1,63 @@
+import { Locations } from "@/constants/props";
+
+export async function getLocations() {
+	const response = await fetch(`${process.env.VITE_BASE_URL}/api/locations`, {
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+
+	if (response.ok) {
+		const data = await response.json();
+		return data.locations as Locations[];
+	}
+
+	return [];
+}
+
+export async function getSpecificColumnData(column: string) {
+	const response = await fetch(
+		`${process.env.VITE_BASE_URL}/api/locations/${column}`,
+		{
+			headers: {
+				"Content-Type": "application/json",
+			},
+		},
+	);
+
+	if (response.ok) {
+		const data = await response.json();
+		return data.locations as string[];
+	}
+
+	return [];
+}
+
+export async function createLocation(location: Locations) {
+	const newLocation = {
+		city: location.city,
+		country: location.country,
+		state: location.state,
+		region: location.region,
+	};
+
+	const response = await fetch(`${process.env.VITE_BASE_URL}/api/locations`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(newLocation),
+	});
+
+	const data = await response.json();
+	if (response.ok) {
+		return {
+			success: true,
+			message: data.message,
+		};
+	}
+	return {
+		success: false,
+		message: data.message ?? "Cannot create new location, please try again",
+	};
+}
